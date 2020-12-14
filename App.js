b7c1f23e955821739d39b953b413474763ff4936
milton <milton@miltons-MacBook.local>
2020-12-14 11:28:28 +0800

    setup main screen with loading indicator

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [Loading, setLoading] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>
+        {Loading ? <ActivityIndicator size="large" color="red" /> : "Loaded"}
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Refresh!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +26,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    marginBottom: 24,
+  },
+  arrivalTime: {
+    fontSize: 64,
+    marginBottom: 32,
+  },
+  button: {
+    padding: 20,
+    backgroundColor: "darkgreen",
+  },
+  buttonText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
   },
 });
